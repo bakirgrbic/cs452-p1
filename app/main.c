91d7c70c9bc4aa27b904ec80fc5e8034a6434c8f
Bakir Grbic <97543350+bakirgrbic@users.noreply.github.com>
2024-09-24 12:09:29 -0600

    Task 4 Complete

--- app/main.c
+++ app/main.c
@@ -1,7 +1,18 @@
 #include "../src/lab.h"
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+// TODO: need to make sure readline and history work on github workspaces
 
 int main(int argc, char *argv[]) {
-  parse_args(argc, argv);
-  return 0;
+    parse_args(argc, argv);
+
+    char *line;
+    using_history();
+    while ((line=readline("$"))){
+        printf("%s\n",line);
+        add_history(line);
+        free(line);
+    }
+    return 0;
 }
